5d8d6472ccc52eab8d2d6be728b4b839ecb7aab2
iLensky <ivan.lensky@gmail.com>
2018-06-12 17:15:41 +0500

    Complete rewrite without using any built-in methods for string matching

--- task2.rb
+++ task2.rb
@@ -2,10 +2,20 @@
 
 # SECOND TASK - Максимальная подстрока
 
-def find_str(working_string)
-    for n in (working_string.length).downto(2)
+def count_substr(working_string, substring)
+    counter = 0
+    for n in 0..(working_string.length - substring.length)
+        if (working_string[n, substring.length] == substring)
+            counter += 1
+        end
+    end
+    return counter
+end
+
+def find_substr(working_string)
+    for n in (working_string.length - 1).downto(1)
         for m in 0..(working_string.length - n)
-            if (working_string.index(working_string[m, n], m + 1) != nil)
+            if (count_substr(working_string, working_string[m, n]) > 1)
                 return working_string[m, n]
             end
         end
@@ -13,8 +23,8 @@ def find_str(working_string)
     return nil
 end
 
-if ((ARGV[0] != nil) && (ARGV[0].length <= 100))
-    result = find_str(ARGV[0])
+if ((ARGV[0] != nil) && (ARGV[0].length > 1) && (ARGV[0].length <= 100) && (ARGV[0].match(/[^[:lower:]]/) == nil))
+    result = find_substr(ARGV[0])
     if (result != nil)
         puts(result)
     end
